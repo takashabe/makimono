3a210fb7bbc70ca8ebd2393dd6828722c67320c8
takashabe <takashabe.public@gmail.com>
2016-02-05 03:12:38 +0900

    slick, wsの最低限動作するコードの雛形をおいた

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,6 +8,12 @@ import play.api.mvc._
 import play.db.NamedDatabase
 import slick.driver.JdbcProfile
 import play.api.Play.current
+import play.Logger
+import play.api.Play.current
+import play.api.libs.ws._
+import play.api.libs.ws.ning.NingAsyncHttpClientConfigBuilder
+import scala.concurrent._
+import scala.concurrent.duration.{ Duration, MILLISECONDS }
 
 import models._
 
@@ -21,6 +27,24 @@ class Application @Inject()(dbConfigProvider: DatabaseConfigProvider) extends Co
   val member = TableQuery[Members]
 
   def index = Action {
+    // slickのスニペット
+//    val query = sql"SELECT * FROM token".as[(Int, String)]
+//    val res = db.run(query)
+//    val query2 = DBIO.seq (
+//      sqlu"INSERT INTO token (token) VALUES ('query2_1')",
+//      sqlu"INSERT INTO token (token) VALUES ('query2_2')"
+//    )
+//    val res2 = db.run(query2)
+
+    // WSのスニペット
+//    val token = "2c231b0d38135b026ee48bbe66515d88"
+//    val feature = WS.url("https://api.chatwork.com/v1/me")
+//      .withHeaders("X-ChatWorkToken" -> token)
+//      .get()
+//
+//    val res = Await.result(feature, Duration(5000, MILLISECONDS))
+//    Logger.debug(res.body)
+
     Ok(views.html.index("Your new application is ready."))
   }
 

871dbadccecf6202021537f91260ac80eae90937
takashabe <takashabe.public@gmail.com>
2016-02-07 10:01:01 +0900

    ひとまず動くところまで

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,8 +12,10 @@ import play.Logger
 import play.api.Play.current
 import play.api.libs.ws._
 import play.api.libs.ws.ning.NingAsyncHttpClientConfigBuilder
+import scala.collection.mutable.ArrayBuffer
 import scala.concurrent._
 import scala.concurrent.duration.{ Duration, MILLISECONDS }
+import scala.concurrent.ExecutionContext.Implicits.global
 
 import models._
 
@@ -68,13 +70,42 @@ class Application @Inject()(dbConfigProvider: DatabaseConfigProvider) extends Co
     * @param id グループID
     */
   def getGroupDetail(id: Int) = Action {
+    Logger.debug("GET /group/detail")
     Ok(views.html.index("Your new application is ready."))
   }
 
   /**
     * グループを作る
     */
-  def postGroupCreate = Action {
+  def postGroupCreate = Action { request =>
+    val formParam = request.body.asFormUrlEncoded.getOrElse(Map[String, Seq[String]]())
+
+    val groupName = formParam.getOrElse("name", None)
+    groupName match {
+      case None =>
+      case x: ArrayBuffer[String] => {
+        val name = x.head
+        val query = DBIO.seq (
+          sqlu"INSERT INTO faction (`name`) VALUES (#$name)"
+        )
+        val res = db.run(query)
+      }
+    }
+
+    val account_ids = formParam.getOrElse("account_id", None)
+    account_ids match {
+      case None =>
+      case x: ArrayBuffer[String] => {
+        // TODO: contacts, factionの結果を元に作る!!!
+        for (id <- x) {
+          val query = DBIO.seq (
+            sqlu"INSERT INTO member (account_id, faction_id, room_id, `name`) VALUES (#$id, 1, 10, 'Dummy')"
+          )
+          val res = db.run(query)
+        }
+      }
+    }
+
     Ok(views.html.index("Your new application is ready."))
   }
 
@@ -84,6 +115,22 @@ class Application @Inject()(dbConfigProvider: DatabaseConfigProvider) extends Co
     * @param id グループID
     */
   def postGroupMessage(id: Int) = Action {
+    val res = db.run(sql"SELECT account_id, room_id FROM member WHERE faction_id=#$id".as[(Int, Int)])
+    res.onSuccess {
+      case s =>
+        Logger.debug(s.toString())
+        for (ids <- s) {
+          val room_id = ids._2
+
+          // ChatworkにPOST
+          val token = "2c231b0d38135b026ee48bbe66515d88"
+          val feature = WS.url("https://api.chatwork.com/v1/rooms/%s/messages".format(room_id))
+            .withHeaders("X-ChatWorkToken" -> token)
+            .post(Map("body" -> Seq("最高の開発合宿")))
+          Logger.debug("room_id=%s".format(room_id))
+        }
+    }
+
     Ok(views.html.index("Your new application is ready."))
   }
 }
